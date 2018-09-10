e1093f6e2287734ea959e4e155624ff100364ca3
SmileWalkman <smilewalkman@sina.com>
2018-09-10 15:42:23 +0800

    Producer,Consumer

--- Algorithm/src/thread/LockCondition.java
+++ Algorithm/src/thread/LockCondition.java
@@ -1,10 +1,98 @@
 package thread;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class LockCondition {
 
+	List<String> plist = new ArrayList<String>();
+	int mark;
+	Lock lock = new ReentrantLock();
+	Condition condition = lock.newCondition();
+
+	public void produce(int mark) {
+		try {
+			
+			Thread.sleep(100);
+			lock.lock();
+			plist.add("\"P" + mark + "\"");
+			System.out.println(mark + " Produce>>" + plist.get(plist.size() - 1) + ": <list=" + plist.size() + ">");
+			condition.signalAll(); 
+//			System.out.println("after");
+		} catch (Exception e) {
+			e.printStackTrace();
+//			System.out.println("Excetion: " + mark+":"+e.getMessage());
+		} finally {
+			lock.unlock();
+		}
+	}
+
+	public void consume(int mark) {
+		try {
+			Thread.sleep(1000);
+			lock.lock();
+//			
+			while (plist.size() <= 0) {
+				condition.await();
+			}
+			int index = plist.size() - 1;
+			System.out.println(mark + ":Consume:" + plist.get(index) + ": Size=" + plist.size());
+			plist.remove(index);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			lock.unlock();
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		LockCondition obj = new LockCondition();
+		for (int i = 0; i < 5; i++) {
+			ProduceThread ct = new ProduceThread(obj, i);
+			ct.start();
+		}
+		for (int i = 1; i < 10; i++) {
+			ConsumeThread pt = new ConsumeThread(obj, i);
+			pt.start();
+		}
 
 	}
 
 }
+
+class ProduceThread extends Thread {
+	LockCondition obj;
+	int mark;
+
+	public ProduceThread(LockCondition obj, int mark) {
+		this.obj = obj;
+		this.mark = mark;
+	}
+
+	public void run() {
+		while (true) {
+			obj.produce(mark);
+		}
+	}
+}
+
+class ConsumeThread extends Thread {
+	LockCondition obj;
+	int mark;
+
+	public ConsumeThread(LockCondition obj, int mark) {
+		this.obj = obj;
+		this.mark = mark;
+	}
+
+	public void run() {
+		while (true) {
+			obj.consume(mark);
+		}
+	}
+}
+
